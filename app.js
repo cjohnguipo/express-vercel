dede972a6730a0d2166ee4366ca890578804e039
Christian John Guipo <cjohnguipo@gmail.com>
2023-01-05 17:41:13 +0800

    commit

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
+app.use("/auth", require("./routes/auth"));
+app.use("/posts", require("./routes/posts"));
 app.use('/', routes);
+
 
 // default catch all handler
 app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
+
 
 module.exports = app;
---
+++ database.js
@@ -0,0 +1,34 @@
+const users = [
+  {
+    email: "john@gmail.com",
+    password: "$2b$10$jgt7BKM3nsIrqNPLFTZ6GOb.m/qpbiBxTEZBFrWYYRn.lIG3jpfWy",
+  },
+  {
+    "email": "tope@gmail.com",
+    "password": "$2b$10$DKv6BIxW3952AYgsWIYV5O1QIQZh0snsgUT53iVoXcuFzWDIWS2Iq"
+  },
+  {
+    "email": "cjohnguipo@gmail.com",
+    "password": "$2b$10$jgt7BKM3nsIrqNPLFTZ6GOb.m/qpbiBxTEZBFrWYYRn.lIG3jpfWy"
+  },
+];
+
+const publicPosts = [
+  {
+    title: "Post 1",
+    content: "Post 1 is free",
+  },
+  {
+    title: "Post 2",
+    content: "Post 2 is free",
+  },
+];
+
+const privatePosts = [
+  {
+    title: "Post 3",
+    content: "Post 3 is private",
+  },
+];
+
+module.exports = { users, publicPosts, privatePosts };
