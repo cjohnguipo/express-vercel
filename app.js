c5ec2c1d584e3d8ab085e5a27f7ca137494a0c66
Christian John Guipo <cjohnguipo@gmail.com>
2023-01-05 18:04:36 +0800

    commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
-// app.use('/', routes);
+app.use('/', routes);
 // app.use("/auth", require("./routes/auth"));
  app.use("/posts", require("./routes/posts"));
 
