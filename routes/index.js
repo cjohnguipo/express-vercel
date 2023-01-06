375747072ebcb924299318908af40e943c973902
Christian John Guipo <cjohnguipo@gmail.com>
2023-01-06 12:19:46 +0800

    commit now

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ const r = Router();
 
 r.use('/demo', demo);
 
-r.get('/', (req, res) => res.json(new SuccessResponseObject('express vercel boiler plate')));
+// r.get('/', (req, res) => res.json(new SuccessResponseObject('express vercel boiler plate')));
+r.get('/', (req, res) => res.html(new HTMLObjectElement( <h1>Welcome !!!</h1>));
+    
 
 module.exports = r;
